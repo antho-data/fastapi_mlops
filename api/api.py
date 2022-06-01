d4186fe56a9edd4b5ed037997781ec06bd990465
antho-data <jaillet.a74@gmail.com>
2022-06-01 13:59:15 +0000

    ssl added

--- api/api.py
+++ api/api.py
@@ -79,4 +79,4 @@ async def get_questions(question: Question, user: any = Depends(get_current_user
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=80)
